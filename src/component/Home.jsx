20a3cfac006a5e3c74772af73bb36ee2e1e74223
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-01-25 18:17:44 +0600

    still learning useEffect

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList/BlogList";
 
 const Home = () => {
@@ -13,12 +13,18 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const remainedBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(remainedBlogs);
   };
 
-  
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
+
   //useEffect hook runs after every render in react.
   //So, we have to be careful to use it to change state. Because it can end up by falling in a continuous loop hole.
   //Generally, it is used to fetch data after every render
@@ -30,6 +36,7 @@ const Home = () => {
         title="All Blogs !"
         handleDelete={handleDelete}
       />
+      <button onClick={() => setName("luigi")}>change name</button>
       {/* <BlogList
         blogList={blogs.filter((blog) => blog.author === "mario")}
         title="Mario's Blog"
