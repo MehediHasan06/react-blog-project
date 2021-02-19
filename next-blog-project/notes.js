901884f745895f73c8ae7955a46e08858627c231
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-02-20 01:11:49 +0600

    automatic generated route

---
+++ next-blog-project/notes.js
@@ -0,0 +1,10 @@
+//To create a next boilerplate -> npx create-next-app project-name
+//To start a next project -> yarn dev
+
+//Page Structure ----------------------------------------------------
+
+//In pages folder, if I add a component,this'll automatically create a route. The name of the route will be same as the file name. For ex: we've created a "about.js" component in the "pages" folder. Now, if we hit http://localhost:3000/about then we can see that component's view. 
+//Again, pages->ninjas->test.js....Then, to access the test page -> http://localhost:3000/ninjas/test
+//Again, pages->ninjas->index.js....Then, to access the test page -> http://localhost:3000/ninjas/. Cause the name "index" create a root path for that file.
+
+/* So, Next js generates routes automatically */
---
+++ next-blog-project/pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (  
+    <>
+      <h1>About</h1>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+    </>
+  );
+}
+ 
+export default About;
