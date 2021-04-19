cadc4da0e7ef4d99aede3fb7b8cdf7760e97ba6b
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-04-20 05:34:30 +0600

    reach-router

---
+++ react-reach-router/src/App.js
@@ -0,0 +1,33 @@
+import { Link, Router } from "@reach/router";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import Invoice from "./components/Invoice";
+
+function App() {
+  return (
+    <div>
+      <h1>Tutorial!</h1>
+      <nav>
+        <Link to="/">Home</Link> 
+        <Link to="dashboard">Dashboard</Link>
+        <Link to="invoices/123">Invoice 123</Link>
+      </nav>
+      {/* 
+      This router is a bit different than the react router. Which is like that,
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch> 
+        So, reach/router simplifies the react-router a bit.
+      */}
+      <Router> 
+        <Home path="/" />
+        <Dashboard path="/dashboard" />
+        <Invoice path="invoices/:invoiceId" />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-reach-router/src/components/Invoice.js
@@ -0,0 +1,7 @@
+const Invoice = (props) => (
+  <div>
+    <h2>Invoice {props.invoiceId}</h2>
+  </div>
+);
+
+export default Invoice;
