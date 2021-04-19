69cd578b25414e75fefdb7b757a20f9db7c630d0
MehediHasan06 <mehedihasanmasud06@gmail.com>
2021-04-20 01:53:18 +0600

    zz

---
+++ react-style-comps/src/App.js
@@ -0,0 +1,12 @@
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello styled Comnponets</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ react-style-comps/src/components/Wrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  margin-top: 2em;
+  padding: 4em;
+  background: papayawhip;
+`;
+
+export default Wrapper;
